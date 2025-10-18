6b73951b35663dfeb977b34183c02a2458ac1504
SaschaKuhlmann <saanku123@t-online.de>
2025-10-18 14:27:30 +0200

    added landing Page and translation for that

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home.component',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
